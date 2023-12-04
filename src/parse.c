063267510fe302a7fda85f4670879638ddda38d3
Tijmen De Bruine <tde-brui@f1r4s6.codam.nl>
2023-12-04 14:06:54 +0100

    new commit

--- src/parse.c
+++ src/parse.c
@@ -1,23 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   parse.c                                            :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: sschelti <sschelti@student.42.fr>            +#+                     */
+/*   By: tde-brui <tde-brui@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/10/02 16:50:56 by tde-brui      #+#    #+#                 */
-/*   Updated: 2023/12/04 13:35:56 by tde-brui      ########   odam.nl         */
+/*   Created: 2023/12/04 13:45:52 by tde-brui      #+#    #+#                 */
+/*   Updated: 2023/12/04 13:58:38 by tde-brui      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include "../libs/mlx/include/MLX42/MLX42.h"
-#include <stdio.h>
-
-#define WIDTH 512
-#define HEIGHT 512
-
-int	main(void)
-{
-	
-}
+#include "../libs/cub3d.h"
